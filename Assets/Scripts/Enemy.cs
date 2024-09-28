863707820ee91d3eb56765908f70a7696c9fad6c
ub3rdevice <brigada1111@mail.ru>
2024-09-28 16:48:40 +0400

    implemented basic particles collision

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    void OnParticleCollision(GameObject other) {
+
+        Debug.Log($"{name} is hit by{other.gameObject.name}");
+        Destroy(gameObject);
+    }
+}
