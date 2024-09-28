33245748dff2937c2c4aac6a904d1fbee854c0a7
ub3rdevice <brigada1111@mail.ru>
2024-09-28 21:17:21 +0400

    implemented score counting

--- Assets/Scripts/ScoreBoard.cs
+++ Assets/Scripts/ScoreBoard.cs
@@ -1,15 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ScoreBoard : MonoBehaviour
 {
     int score;
     
+    TMP_Text scoreTextField;
+
+    void Start()
+    {
+        scoreTextField = GetComponent<TMP_Text>();
+        scoreTextField.text = "Let the carnage begin!";
+    }
+
     public void IncreaseScore(int amountToIncrase) 
     {
         score += amountToIncrase; // a = a + b equals a += b;
-        Debug.Log($"Your score is {score}");
+        scoreTextField.text = score.ToString(); // casting int to string
     }
 
 }
