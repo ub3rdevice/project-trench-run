b6eb98f513d38f3302ac14afc5ef6a0f1066dd7b
ub3rdevice <brigada1111@mail.ru>
2024-09-29 22:35:35 +0400

    added SFX for player explosion

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -9,14 +9,26 @@ public class CollisionHandler : MonoBehaviour
 {
     [SerializeField] ParticleSystem explosionParticles;
     [SerializeField] float lvlLoadDelay = 3f;
+    [SerializeField] AudioClip deathSFX;
+
+    AudioSource _audiosource;
+
+    bool isInTransition = false;
 
     // void OnTriggerEnter(Collider other) { // in case we want to trigger something particular after we collide with object
 
     //     StartCrashSequence();
 
     // }
+    void Start() 
+    {
+        _audiosource = GetComponent<AudioSource>();
+    }
+
 
     void OnCollisionEnter(Collision other) {
+        
+        if (isInTransition) { return;}
 
         StartCrashSequence();
         
@@ -30,6 +42,10 @@ public class CollisionHandler : MonoBehaviour
 
     void StartCrashSequence() 
     {
+        isInTransition = true;
+        _audiosource.Stop();
+        _audiosource.PlayOneShot(deathSFX);
+
         explosionParticles.Play();
         GetComponent<PlayerControls>().enabled = false;
         GetComponent<MeshRenderer>().enabled = false;
